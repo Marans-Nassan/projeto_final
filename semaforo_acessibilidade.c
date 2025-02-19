fa5ac76395673ba71a0d88ee35425dbef6a05bbb
Hugo <hugomaranse@gmail.com>
2025-02-19 20:35:42 -0300

    Configurado os leds e o buzzer. Agora podemos iniciar o uso do i2c e do Oled.

--- semaforo_acessibilidade.c
+++ semaforo_acessibilidade.c
@@ -8,32 +8,33 @@
 #define botao_a 5
 #define botao_b 6
 #define buzzer_a 21
-#define divisor 32
-#define periodo 14909
+#define divisor 125.0
+#define periodo 999
 
 static bool true_false = 1;
 uint8_t pinos[3] = {11, 13, 21};
 static uint8_t verde = led_verde;
 static uint8_t vermelho = led_vermelho;
+static uint8_t slice;
 
 void led_e_buz_init();
 void botinit();
 void gpio_irq_handler(uint gpio, uint32_t events);
 int64_t turn_off_leds(alarm_id_t id, void *user_data);
+int64_t turn_off_pwm(alarm_id_t id, void *user_data);
+int64_t turn_on_pwm(alarm_id_t id, void *user_data);
 void alternando_interrupcao();
-void pwm_setup();
-void pwm_level(uint32_t duty_cycle);
+void alarm_buzzers();
+void pwm_setup(uint32_t duty_cycle);
 
 int main(){
 
 led_e_buz_init();
 botinit();
-pwm_setup();
-pwm_level(0);
+pwm_setup(0);
 gpio_set_irq_enabled_with_callback(botao_a, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
 gpio_set_irq_enabled_with_callback(botao_b, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
-
-    stdio_init_all();
+stdio_init_all();
 
     while (true) {
 
@@ -49,7 +50,6 @@ void led_e_buz_init(){
 
 }
 
-
 void botinit(){
     for(uint8_t botoes = 5 ; botoes < 7 ; botoes++){
         gpio_init(botoes);
@@ -59,27 +59,24 @@ void botinit(){
 }
 
 void gpio_irq_handler(uint gpio, uint32_t events){
-    uint64_t current_time = to_us_since_boot(get_absolute_time())* 1000000;
-    uint64_t last_time = 0;
-
-    if(gpio == botao_a || gpio == botao_b && current_time - last_time > 3){
+    if(gpio == botao_a || gpio == botao_b) {
         if(gpio == botao_a){
             gpio_put(led_verde, 1);
             gpio_put(led_vermelho, 1);
-            alternando_interrupcao();
-            add_alarm_in_ms(3000, turn_off_leds, &verde, false);
-            add_alarm_in_ms(6000, turn_off_leds, &vermelho, false);
+            alternando_interrupcao();           
+            add_alarm_in_ms(15000, turn_off_leds, &verde, false);
+            add_alarm_in_ms(30000, turn_off_leds, &vermelho, false);
         }
         if(gpio == botao_b){
+            pwm_set_wrap(slice, 999);
             gpio_put(led_verde, 1);
             gpio_put(led_vermelho, 1);
             alternando_interrupcao();
-            
-            add_alarm_in_ms(3000, turn_off_leds, &verde, false);
-            add_alarm_in_ms(6000, turn_off_leds, &vermelho, false);
+            alarm_buzzers();
+            add_alarm_in_ms(20000, turn_off_leds, &verde, false);
+            add_alarm_in_ms(60000, turn_off_leds, &vermelho, false);
         }
     }
-    last_time = current_time;
 }
 
 int64_t turn_off_leds(alarm_id_t id, void *user_data){
@@ -89,6 +86,21 @@ if(gpio_get(vermelho) == 0)alternando_interrupcao();
 return 0;
 }
 
+int64_t turn_off_pwm(alarm_id_t id, void *user_data){
+pwm_set_gpio_level(buzzer_a, 0);
+return 0;
+}
+
+int64_t turn_on_pwm(alarm_id_t id, void *user_data){
+if(gpio_get(verde) == 1)pwm_set_gpio_level(buzzer_a, 100);       
+    else{
+        pwm_set_wrap(slice, 2999);
+        pwm_set_gpio_level(buzzer_a, 100);
+    }
+
+return 0;    
+}
+
 void alternando_interrupcao(){
     true_false = !true_false;
     
@@ -96,15 +108,27 @@ void alternando_interrupcao(){
     gpio_set_irq_enabled_with_callback(botao_b, GPIO_IRQ_EDGE_FALL, true_false, gpio_irq_handler);
 }
 
-void pwm_setup(){
-    uint8_t slice;
+void pwm_setup(uint32_t duty_cycle){
     gpio_set_function(buzzer_a, GPIO_FUNC_PWM);
     slice = pwm_gpio_to_slice_num(buzzer_a);
     pwm_set_clkdiv(slice, divisor);
     pwm_set_wrap(slice, periodo);
-    pwm_set_enabled(slice, false);
+    pwm_set_gpio_level(buzzer_a, periodo * duty_cycle / 100);
+    pwm_set_enabled(slice, true);
 }
 
-void pwm_level(uint32_t duty_cycle){
-    pwm_set_gpio_level(buzzer_a, periodo * duty_cycle / 100);
+void alarm_buzzers(){
+pwm_set_gpio_level(buzzer_a, 50);
+add_alarm_in_ms(200, turn_off_pwm, NULL, false);
+add_alarm_in_ms(15000, turn_on_pwm, NULL, false);
+add_alarm_in_ms(16000, turn_off_pwm, NULL, false);
+add_alarm_in_ms(17000, turn_on_pwm, NULL, false);
+add_alarm_in_ms(18000, turn_off_pwm, NULL, false);
+add_alarm_in_us(20000040, turn_on_pwm, NULL, false);
+add_alarm_in_ms(20600, turn_off_pwm, NULL, false);
+add_alarm_in_ms(50000, turn_on_pwm, NULL, false);
+add_alarm_in_ms(52000, turn_off_pwm, NULL, false);
+add_alarm_in_ms(54000, turn_on_pwm, NULL, false);
+add_alarm_in_ms(56000, turn_off_pwm, NULL, false);
+
 }
